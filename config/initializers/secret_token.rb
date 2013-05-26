d34d5daa99361fdad2adf0ffeb48457e76c49b40
Saron Yitbarek <megabedd@gmail.com>
2013-05-25 20:13:09 -0400

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Megabedd::Application.config.secret_token = '4123e3614b04c1742ffce2c7b1eb35ebc4b7c48ff0144b6a92706d841d212eec7c861abfb7aa6c1ec45d258e97960bb4c2d6376b34c1c125599c4732e2d1c423'
